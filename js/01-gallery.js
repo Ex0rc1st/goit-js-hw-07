9ecc95b5f0ded497b06bdb10c664b051e6a34bbb
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-08-08 19:50:47 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems.map(galleryItem =>
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
+            <img
+            class="gallery__image"
+            src="${galleryItem.preview}"
+            data-source="${galleryItem.original}"
+            alt="${galleryItem.description}"
+            />
+        </a>
+    </div>`).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+
+galleryRef.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return
+    }
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    document.addEventListener('keydown', e => {
+        if (e.key === 'Escape') {
+            instance.close()
+        }
+    })
+})
