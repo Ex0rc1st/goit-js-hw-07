4064b8c7408dcde305ed5017db196ac9a1c3009d
Anton Salnykov <anton.salnikov2010@gmail.com>
2022-08-08 20:52:28 +0300

    Task 1, 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup = galleryItems.map(galleryItem =>
+    `<a class="gallery__link" href="${galleryItem.original}">
+        <img
+        class="gallery__image"
+        src="${galleryItem.preview}"
+        data-source="${galleryItem.original}"
+        alt="${galleryItem.description}"
+        />
+    </a>
+    `).join('');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+	captionDelay: 250
+})
